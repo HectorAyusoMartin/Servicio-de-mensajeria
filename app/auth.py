9f9aeb4a5b2cdbe7093dfa2d67752e7f67737dc5
HectorAyusoMartin <hector.ayuso.martin@gmail.com>
2025-03-19 16:36:44 +0100

    Añadida funcion de hasheo de pass plana y función de verificaion de pass plana con su correspondiente hash. Todo ello en auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,27 @@
 """
-Funciones y endpoints para autenticación
+Funciones y endpoints para manejar la lógica de la autenticación
 
 """
+
+from passlib.context import CryptContext
+
+
+#!Configuramos Bcrypt como algortimo de hashing
+pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
+
+
+
+
+def get_password_hash(password:str)->str:
+    
+    """Devuelve un hash de la contraseña proporcionada en formato string"""
+    
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password:str,hashed_password:str)->bool:
+    
+    """Compara la clave en texto plano con su hash y devuelve True si la verificación es positiva."""
+    
+    return pwd_context.verify(plain_password,hashed_password)
+
