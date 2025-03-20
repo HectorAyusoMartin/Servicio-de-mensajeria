ece4b66875ccd2a6c1c7c92278843b3655ded957
HectorAyusoMartin <hector.ayuso.martin@gmail.com>
2025-03-20 13:20:42 +0100

    Modificado el modelo de messages

--- app/models/messages.py
+++ app/models/messages.py
@@ -8,8 +8,8 @@ from datetime import datetime
 
 class Message(BaseModel):
     
-    username:str
-    message:str
+    username:str #!Usuario que envia el mensaje
+    message:str #!Contenido del mensaje
     timestamp:datetime = datetime.utcnow() #! Esto hace un timestamp del tiempo actual
     
     model_config = ConfigDict(extra="forbid") #! Esto bloquea campos adicionales a los establecidos aqui.
